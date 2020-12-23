7319ba2792c3f01b463bffa79fb211f4f7997c02
Ibrahim Hassaan <ibr.hassaan@gmail.com>
2020-12-23 11:06:53 +0500

    emp fixed

--- app/Jobs/ZKTSync.php
+++ app/Jobs/ZKTSync.php
@@ -73,7 +73,7 @@ class ZKTSync implements ShouldQueue
     }
 
     public function getAttendance($from=null,$to=null,$external_id=null){
-        $logs=$this->db->table('CHECKINOUT')->where('CHECKTIME','>',$from)->addSelect(['USERID'=>$this->db->table('USERINFO')->select('BadgeNumber')->whereColumn('CHECKINOUT.USERID', 'USERINFO.USERID')->limit(1)]);
+        $logs=$this->db->table('CHECKINOUT')->where('CHECKTIME','>',$from)->addSelect(['USERID'=>$this->db->table('USERINFO')->select('SSN')->whereColumn('CHECKINOUT.USERID', 'USERINFO.USERID')->limit(1)]);
         if($to){
             $logs=$logs->where('ChHECKTIME','<=',$to);
         }
@@ -82,7 +82,7 @@ class ZKTSync implements ShouldQueue
                 $query->select(DB::raw(1))
                       ->from('USERINFO')
                       ->whereColumn('USERINFO.USERID', 'CHECKINOUT.USERID')
-                      ->where('USERINFO.BadgeNumber',$external_id);
+                      ->where('USERINFO.SSN',$external_id);
             });
         }
 
