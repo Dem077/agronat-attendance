4870e3be3df28ad17d427aed589d7facdf673908
Ibrahim Hassaan <ibr.hassaan@gmail.com>
2021-03-28 11:34:49 +0500

    attendance report fix

--- app/Http/Livewire/AttendanceReport.php
+++ app/Http/Livewire/AttendanceReport.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\Attendance;
 use App\Models\User;
 use DateTime;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
@@ -47,7 +48,7 @@ class AttendanceReport extends Component
         $day=24;
         $this->end_date=(new DateTime("{$year}-{$month}-{$day}"))->format('Y-m-d');
     }
-    public function exportRecord(){
+    public function exportRecord2(){
         $attendances=Attendance::select('user_id','ck_date','late_min','status')->addSelect(['employee' => User::select('name')->whereColumn('user_id', 'users.id')->limit(1)]);
         if($this->user_id){
             $attendances=$attendances->where('user_id','=',$this->user_id);
@@ -132,6 +133,42 @@ class AttendanceReport extends Component
         $filename = sprintf('%1$s-%2$s-%3$s', str_replace(' ', '', 'attendances'), date('Ymd'), date('His'));
 
         return export_csv2($header, array_values($report), $filename);
+    }
+    public function exportRecord(){
+        $attendances=Attendance::select(DB::raw("user_id,status,count(1) as count, sum(late_min) as late_min"))->addSelect(['employee' => User::select('name')->whereColumn('user_id', 'users.id')->limit(1)]);
+        if($this->user_id){
+            $attendances=$attendances->where('user_id','=',$this->user_id);
+        }
+
+        if($this->start_date){
+            $attendances=$attendances->where('ck_date','>=',$this->start_date);
+        }
+        $this->end_date=$this->end_date?$this->end_date:(new \DateTime())->format('Y-m-d');
+        if($this->end_date){
+            $attendances=$attendances->where('ck_date','<=',$this->end_date);
+        }
+        $attendances=$attendances->groupBy(['user_id','status'])
+                    ->get();
+        
+        $report=[];
+        $header=['name'=>'employee','late min'=>'late_min','Normal'=>'Present'];
+
+        foreach($attendances as $att){
+            if($att->status=='Holiday'){
+                continue;
+            }
+            if(!in_array($att->status,$header)){
+                $header[$att->status]=$att->status;
+            }
+            if(!isset($report[$att->user_id])){
+                $report[$att->user_id]=Arr::only($att->toArray(),['user_id','employee']);
+                $report[$att->user_id]['late_min']=0;
+            }
+            $report[$att->user_id][$att->status]=$att->count;
+            $report[$att->user_id]['late_min']+=$att->late_min;
+        }
+        $filename = sprintf('%1$s-%2$s-%3$s', str_replace(' ', '', 'attendances'), date('Ymd'), date('His'));
+        return export_csv($header, array_values($report), $filename);
     }
 
 }
