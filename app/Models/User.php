d6cfec2c66e98292c7988ad4f37fdeb8fd5a98cd
unknown <ibr.hassaan@gmail.com>
2021-05-23 11:46:18 +0500

    user leaves added

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,4 +71,8 @@ class User extends Authenticatable
     public function department(){
         return $this->belongsTo(Department::class);
     }
+
+    public function leaves(){
+        return $this->hasMany(Leave::class);
+    }
 }
