43c7aa8289848b34df10c055244333ce161d6d5f
unknown <ibr.hassaan@gmail.com>
2022-01-02 08:51:41 +0500

    month selection fixed

--- app/Http/Livewire/Partials/AttendancePeriod.php
+++ app/Http/Livewire/Partials/AttendancePeriod.php
@@ -7,7 +7,8 @@ use Livewire\Component;
 
 class AttendancePeriod extends Component
 {
-    public $periods,$period_id;
+    public $periods;
+    public $period_id=0;
     public function render()
     {
         $this->periods=$this->getPeriods();
@@ -16,24 +17,28 @@ class AttendancePeriod extends Component
     }
 
     public function getPeriods(){
-        $today=new DateTime();
-        $year=$today->format("Y");
-        for($i=1;$i<=12;$i++){
-            $end=new DateTime("{$year}-{$i}-24");
-            $month=$end->format("F");
-            $start=(clone $end)->modify("last month");
-            $period[]=[
-                "month"=>$month,
-                "start"=>$start->format("Y-m-25"),
-                "end"=>$end->format('Y-m-d')
-            ];
 
-            if(!$this->period_id){
-                if($today->format('m')==$end->format('m')){
-                    $this->period_id=count($period)-1;
-                }
-            }
+        //current period
+        $current=new DateTime();
+        if((int)$current->format('d')>24){
+            $current->modify('next month');
         }
+        $end=new DateTime($current->format('Y-m-24'));
+        $start=$start=(clone $end)->modify("last month");
+        $period[]=[
+            "month"=>"Current (".$end->format('M, y').")",
+            "start"=>$start->format("Y-m-25"),
+            "end"=>$end->format('Y-m-d')
+        ];
+
+        //last period
+        $end=new DateTime($start->format('Y-m-24'));
+        $start=(clone $end)->modify("last month");
+        $period[]=[
+            "month"=>"Last (".$end->format('M, y').")",
+            "start"=>$start->format("Y-m-25"),
+            "end"=>$end->format('Y-m-d')
+        ];
 
         return  $period;
     }
