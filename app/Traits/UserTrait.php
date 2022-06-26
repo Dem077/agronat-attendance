6d1bb6bdfd24fb09b3dcf848fff5fca0a57b423b
ibrahimhassaan <ibr.hassaan@gmail.com>
2022-06-26 14:54:18 +0500

    emp id added to user select

--- app/Traits/UserTrait.php
+++ app/Traits/UserTrait.php
@@ -15,14 +15,14 @@ trait UserTrait {
         $user=Auth::user();
         $departments=Department::where('supervisor_id',$user->id)->pluck('id')->toArray();
         if(auth()->user()->can('reporting-manager')){
-            $this->users=User::select('name','id')->orderBy('name','asc')->get()->toArray();
+            $this->users=User::select('name','id','emp_no')->orderBy('name','asc')->get()->toArray();
         }elseif($departments){
-            $this->users=User::select('name','id')->whereIn('department_id',$departments)->orderBy('name','asc')->get()->toArray();
+            $this->users=User::select('name','id','emp_no')->whereIn('department_id',$departments)->orderBy('name','asc')->get()->toArray();
             if(!in_array($user->id,$this->users)){
-                array_unshift($this->users,['name'=>$user->name,'id'=>$user->id]);
+                array_unshift($this->users,['name'=>$user->name,'id'=>$user->id,'emp_no'=>$user->emp_no]);
             }
         }else{
-            $this->users=[['id'=>$user->id,'name'=>$user->name]];
+            $this->users=[['id'=>$user->id,'name'=>$user->name,'emp_no'=>$user->emp_no]];
             $this->user_id=$user->id;
         }
     }
--- resources/views/livewire/partials/user-select.blade.php
+++ resources/views/livewire/partials/user-select.blade.php
@@ -4,7 +4,7 @@
     <select type="text" class="form-control" id="{{$ref}}" placeholder="Employee">
         <option value=''>Select Employee</option>
         @foreach($users as $user)
-            <option value='{{$user['id']}}' {{$user_id==$user['id']?'SELECTED':''}}>{{$user['name']}}</option>
+            <option value='{{$user['id']}}' {{$user_id==$user['id']?'SELECTED':''}}>{{$user['name']}} ({{$user['emp_no']}})</option>
         @endforeach
     </select>
 </div>
