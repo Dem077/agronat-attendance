8963e41bd924906f4584101e2d1fc3dac837369e
agronationaldev <it@agronational.mv>
2024-06-23 15:17:06 +0500

    login additional methods added

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -8,7 +8,9 @@ use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
 class FortifyServiceProvider extends ServiceProvider
 {
     /**
@@ -28,6 +30,18 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)
+                        ->orWhere('nid', $request->email) 
+                        ->orWhere('emp_no', $request->email) 
+                        ->first();
+    
+            if ($user && Hash::check($request->password, $user->password)) {
+                return $user;
+            }
+    
+            return null;
+        });
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
