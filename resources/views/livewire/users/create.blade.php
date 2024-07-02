068380c2eee1ef8a206f0ce3d57f11ee2935c9d2
agronationaldev <it@agronational.mv>
2024-07-02 11:02:33 +0500

    BugFix - Added joining date for add employee

--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -13,19 +13,19 @@
            <div class="modal-body">
             <form>
                 <div class="form-group">
-                    <label for="name">fullname:</label>
-                    <input type="text" class="form-control" id="name" wire:model="name" placeholder="name"/>
+                    <label for="name">Fullname:*</label>
+                    <input type="text" class="form-control" id="name" wire:model="name" placeholder="Fullname"/>
                     @error('name') <span class="text-danger">{{ $message }}</span>@enderror
                 </div>
                 <div class="form-group">
-                    <label for="name">national ID / Passport:</label>
-                    <input type="text" class="form-control" id="nid" wire:model="nid" placeholder="national ID"/>
+                    <label for="name">National ID / Passport:*</label>
+                    <input type="text" class="form-control" id="nid" wire:model="nid" placeholder="National ID / Passport"/>
                     @error('nid') <span class="text-danger">{{ $message }}</span>@enderror
                 </div>
                 <div class="form-group">
-                    <label for="department_id">department:</label>
+                    <label for="department_id">Department:</label>
                         <select name="department_id" id="department_id" class="form-control" wire:model="department_id">
-                            <option value="">Select department</option>
+                            <option value="">Select Department</option>
                             @foreach ($departments as $department)
                                 <option value="{{$department->id}}" >{{$department->name}}</option>
                             @endforeach
@@ -44,9 +44,9 @@
                         @error('supervisor_id') <span class="text-danger">{{ $message }}</span>@enderror
                 </div>
                 <div class="form-group">
-                    <label for="location_id">location:</label>
+                    <label for="location_id">Location:</label>
                         <select name="location_id" id="location_id" class="form-control" wire:model="location_id">
-                            <option value="">Select location</option>
+                            <option value="">Select Location</option>
                             @foreach ($locations as $location)
                                 <option value="{{$location->id}}" >{{$location->name}}</option>
                             @endforeach
@@ -55,22 +55,27 @@
 
                 </div>
                 <div class="form-group">
-                    <label for="designation">designation:</label>
-                    <input type="text" class="form-control" id="designation" wire:model="designation" placeholder="designation"/>
+                    <label for="designation">Designation:*</label>
+                    <input type="text" class="form-control" id="designation" wire:model="designation" placeholder="Designation"/>
                     @error('designation') <span class="text-danger">{{ $message }}</span>@enderror
                 </div>
                 <div class="form-group">
-                    <label for="email">email:</label>
-                    <input type="email" class="form-control" id="email" wire:model="email" autocomplete="off" placeholder="email"/>
+                    <label for="joined_date">Joined Date:*</label>
+                    <input type="date" class="form-control" id="joined_date" wire:model="joined_date" placeholder="Joined Date"/>
+                    @error('joined_date') <span class="text-danger">{{ $message }}</span>@enderror
+                </div> 
+                <div class="form-group">
+                    <label for="email">Email:*</label>
+                    <input type="email" class="form-control" id="email" wire:model="email" autocomplete="off" placeholder="Email"/>
                     @error('email') <span class="text-danger">{{ $message }}</span>@enderror
                 </div>
                 <div class="form-group">
-                    <label for="emp_no">emp no:</label>
-                    <input type="text" class="form-control" id="emp_no" wire:model="emp_no" placeholder="emp no"/>
+                    <label for="emp_no">Emp No:*</label>
+                    <input type="text" class="form-control" id="emp_no" wire:model="emp_no" placeholder="Emp no"/>
                     @error('emp_no') <span class="text-danger">{{ $message }}</span>@enderror
                 </div>
                 <div class="form-group">
-                    <label for="gender">Gender:</label>
+                    <label for="gender">Gender:*</label>
                     <select class="form-control" id="gender" wire:model="gender">
                         <option value="">Select Gender</option>
                         <option value="M">Male</option>
@@ -80,19 +85,19 @@
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
-                        <label for="mobile">mobile:</label>
-                        <input type="text" class="form-control" id="mobile" wire:model="mobile" placeholder="mobile"/>
+                        <label for="mobile">Mobile:</label>
+                        <input type="text" class="form-control" id="mobile" wire:model="mobile" placeholder="Mobile"/>
                         @error('mobile') <span class="text-danger">{{ $message }}</span>@enderror
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="phone">phone:</label>
-                        <input type="text" class="form-control" id="phone" wire:model="phone" placeholder="phone"/>
+                        <label for="phone">Phone:</label>
+                        <input type="text" class="form-control" id="phone" wire:model="phone" placeholder="Phone"/>
                         @error('phone') <span class="text-danger">{{ $message }}</span>@enderror
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
-                        <label for="password">Password:</label>
+                        <label for="password">Password:*</label>
                         <input type="password" class="form-control" id="password" wire:model="password" autocomplete="off" placeholder="Password"/>
                         @error('password') <span class="text-danger">{{ $message }}</span>@enderror
                     </div>
